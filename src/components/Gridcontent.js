177abdbb8ddd5e586831ed2df9f481872fd2f5d7
Gopalgarg01 <garggopal554@gmail.com>
2023-04-16 12:49:53 +0530

    commit

---
+++ src/components/Gridcontent.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Gridcontent.css"
+
+export default function Gridcontent() {
+  return (
+    <>
+        <div className='container'>
+
+        
+        <div className='fir'>
+            <h5>Architecure Details</h5>
+            
+        </div >
+        <div className='fir'>
+            <h5>Architecure Details</h5>
+
+        </div>
+        <div className='fir'>
+            <h5>Architecure Details</h5>
+
+        </div>
+        <div className='fir'>
+            <h5>Architecure Details</h5>
+
+        </div>
+        </div>
+    </>
+  )
+}
